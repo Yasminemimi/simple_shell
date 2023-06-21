b9cdc4d132768e133a17b5de3286916bf7dd258d
Akusika <akusikabuatsi96@gmail.com>
2023-06-21 16:13:52 -0700

    Betty code

---
+++ Betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+/**
+  * main - entry point
+  * description: a function that prints a name
+  * Return: always 0
+  */
+
+int main(void)
+{
+	printf("Hello Deborah, \n");
+
+	return (0);
+}
